8823ada981e8e442138ac1c37ffc30a3f1557e2d
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-03-11 11:08:06 -0600

    other commit

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-const button = document.querySelector('button');
-const body =    document.querySelector('body');
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple'];
+    const button = document.querySelector('button');
+    const body =    document.querySelector('h1');
+    const colors = ['orange', 'green', 'blue', 'yellow', 'pink', 'purple'];
 
 
-function changeBackground(){
-    const colorIndex= parseInt(Math.random()*colors.length);
-    body.style.backgroundColor = colors[colorIndex];
-}
+    function changeBackground(){
+        const colorIndex= parseInt(Math.random()*colors.length);
+        body.style.backgroundColor = colors[colorIndex];
+    }
 
 
-body.style.backgroundColor = 'purple';
-button.addEventListener("click", changeBackground);
+    body.style.backgroundColor = 'orange';
+    button.addEventListener("click", changeBackground);
+
